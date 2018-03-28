f3d58fa00ce493545da7f4d099d965a5499802e4
Brittany Barrow <bangelabarrow@gmail.com>
2018-03-28 19:03:54 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -7,5 +7,5 @@ function updatePlaylist(playlist, newArtistName, newSongTitle) {
 
 function removeFromPlaylist(playlist, artistName) {
   delete playlist[artistName];
-  playlist;
+  return playlist;
 }
